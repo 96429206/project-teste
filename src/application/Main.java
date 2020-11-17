f23d8de07ccdc4e50af7a40c35470861c69bad0c
96429206 <vinizanonato@hotmail.com>
2020-11-17 14:07:26 -0300

    mais msgs inserido novo commit

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,10 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 		System.out.println("Opa");
 		System.out.println("Novo commit");
-		
+		System.out.println("Mais um novo commit");
 	}
-
 }
